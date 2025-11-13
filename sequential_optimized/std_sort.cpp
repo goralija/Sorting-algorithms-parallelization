9ac3f8c0806cab24ba8cf8c15d0074a69ac7a070
belmin19341 <bdurmo1@etf.unsa.ba>
2025-11-13 01:04:21 +0100

    small changes in mergesort optimized version

--- sequential_optimized/std_sort.cpp
+++ sequential_optimized/std_sort.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include "main_template.hpp"
+#include <omp.h>
 
 // std::sort wrapper koji koristi obični single-threaded sort bez AVX i bez paralelizma
 void std_sort_wrapper(std::vector<int> &vec)
@@ -11,5 +12,6 @@ void std_sort_wrapper(std::vector<int> &vec)
 
 int main(int argc, char *argv[])
 {
+    omp_set_num_threads(1);
     return run_sort("std_sort", "sequential", std_sort_wrapper, argc, argv);
 }
