e4c1ac21f577b178381fcce44d5c1c75bf9a478b
belmin19341 <bdurmo1@etf.unsa.ba>
2025-11-18 17:48:39 +0100

    merge optimized and parallelized

--- sequential_optimized/merge_sort.cpp
+++ sequential_optimized/merge_sort.cpp
@@ -1,69 +1,103 @@
 #include <vector>
 #include <algorithm>
-#include <cstring>  // for memcpy
+#include <cstring> // for memcpy
 #include <main_template.hpp>
 
-const int INSERTION_SORT_THRESHOLD = 16; //empirijski odabir
+const int INSERTION_SORT_THRESHOLD = 16; // empirijski odabir
 
-void insertion_sort(int arr[], int l, int r) {
-    for (int i = l + 1; i <= r; i++) {
+void insertion_sort(int arr[], int l, int r)
+{
+    for (int i = l + 1; i <= r; i++)
+    {
         int key = arr[i];
         int j = i - 1;
-        while (j >= l && arr[j] > key) {
+        while (j >= l && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j--;
         }
+
         arr[j + 1] = key;
     }
 }
 
-// Merge from arr[l..m] and arr[m+1..r] into temp[l..r], then copy back
-void merge_with_temp(int arr[], int l, int m, int r, int temp[]) {
-    int i = l;
-    int j = m + 1;
-    int k = l;
-    while (i <= m && j <= r) {
-        if (arr[i] <= arr[j]) {
-            temp[k++] = arr[i++];
-        } else {
-            temp[k++] = arr[j++];
-        }
+// Merge dva segmenta arr[l..m] i arr[m+1..r] u temp[l..r]
+void merge(int src[], int dest[], int l, int m, int r)
+{
+    int i = l, j = m + 1, k = l;
+    while (i <= m && j <= r)
+    {
+        if (src[i] <= src[j])
+            dest[k++] = src[i++];
+        else
+            dest[k++] = src[j++];
     }
-    // copy remainder
-    while (i <= m) temp[k++] = arr[i++];
-    while (j <= r) temp[k++] = arr[j++];
-    // copy back
-    memcpy(arr + l, temp + l, (r - l + 1) * sizeof(int));
+    while (i <= m)
+        dest[k++] = src[i++];
+    while (j <= r)
+        dest[k++] = src[j++];
 }
 
-// Top-down recursive, but using temp buffer
-void merge_sort_rec(int arr[], int l, int r, int temp[]) {
-    if (r - l + 1 <= INSERTION_SORT_THRESHOLD) {
-        insertion_sort(arr, l, r);
-        return;
-    }
-    int m = l + (r - l) / 2;
-    merge_sort_rec(arr, l, m, temp);
-    merge_sort_rec(arr, m + 1, r, temp);
-    // optional: if arr[m] <= arr[m+1], već spojeno, skip merge
-    if (arr[m] <= arr[m + 1]) {
-        return;
-    }
-    merge_with_temp(arr, l, m, r, temp);
-}
-
-void merge_sort_opt(int arr[], int n) {
-    // temp buffer
+// Bottom-up iterativni merge sort s ping-pong temp bufferom
+void merge_sort_opt(int arr[], int n)
+{
     int *temp = new int[n];
-    merge_sort_rec(arr, 0, n - 1, temp);
+    int *src = arr;
+    int *dest = temp;
+
+    // Prvo sortiraj male segmente insertion sort-om
+    for (int i = 0; i < n; i += INSERTION_SORT_THRESHOLD)
+    {
+        int r = std::min(i + INSERTION_SORT_THRESHOLD - 1, n - 1);
+        insertion_sort(src, i, r);
+    }
+
+    // Bottom-up merge
+    for (int width = INSERTION_SORT_THRESHOLD; width < n; width *= 2)
+    {
+        for (int i = 0; i < n; i += 2 * width)
+        {
+            int l = i;
+            int m = std::min(i + width - 1, n - 1);
+            int r = std::min(i + 2 * width - 1, n - 1);
+            if (m < r)
+            {
+                // Skip merge ako su već sortirani
+                if (src[m] <= src[m + 1])
+                {
+                    memcpy(dest + l, src + l, (r - l + 1) * sizeof(int));
+                }
+                else
+                {
+                    merge(src, dest, l, m, r);
+                }
+            }
+            else
+            {
+                // samo kopiraj ostatak
+                memcpy(dest + l, src + l, (r - l + 1) * sizeof(int));
+            }
+        }
+        // swap src i dest
+        std::swap(src, dest);
+    }
+
+    // Ako je posljednji rezultat u temp, kopiraj nazad u arr
+    if (src != arr)
+    {
+        memcpy(arr, src, n * sizeof(int));
+    }
+
     delete[] temp;
 }
 
-void merge_sort_wrapper(std::vector<int>& vec) {
+// Wrapper za std::vector
+void merge_sort_wrapper(std::vector<int> &vec)
+{
     merge_sort_opt(vec.data(), (int)vec.size());
 }
 
-
-int main(int argc, char* argv[]) {
-    return run_sort("merge_sort", "sequential", merge_sort_wrapper, argc, argv);
+int main(int argc, char *argv[])
+{
+    return run_sort("merge_sort_optimized", "sequential", merge_sort_wrapper, argc, argv);
 }
